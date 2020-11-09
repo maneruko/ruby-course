f9bb2b4961309bf65189b9ed6a6e62c8e0545b04
maneruko <74158091+maneruko@users.noreply.github.com>
2020-11-09 16:58:49 -0500

    Update montys-color.rb

--- montys-color.rb
+++ montys-color.rb
@@ -12,7 +12,7 @@ end
 
 # Lesson 5: Adding an Else Statement
 
-if color == "yellow"
+if color == "blue"
      puts "Wow! My favorite color is #{color} also!"
 else
      puts "I'm not a big fan of #{color}."
